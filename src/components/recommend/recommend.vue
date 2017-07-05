e6383168f55a1c05884fd85c6eaa12c08a8c7ba9
Xun.Fu <fuxun@xiaoyouzi.com>
2017-07-05 20:22:09 +0800

    完成路由配置和首页tab栏。

---
+++ src/components/recommend/recommend.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    推荐组件
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+
+  }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "~common/stylus/variable"
+  @import "~common/stylus/mixin"
+
+</style>
