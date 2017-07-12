721fc3cdf6e293d44de3f58b02fcff963bfad72e
Xun.Fu <fuxun@xiaoyouzi.com>
2017-07-12 17:03:02 +0800

    完成歌手详情页歌单列表的组件编写。

--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -1,6 +1,6 @@
 <template>
   <transition name="slide">
-    <div class="singer-detail"></div>
+    <music-list :songs="songs" :title="title" :bg-image="bgImage"></music-list>
   </transition>
 </template>
 
@@ -8,12 +8,28 @@
   import {mapGetters} from 'vuex'
   import {getSingerDetail} from 'api/singer'
   import {ERR_OK} from 'api/config'
+  import {createSong} from 'common/js/song'
+  import MusicList from 'components/music-list/music-list'
 
   export default {
+    components: {
+      MusicList
+    },
     computed: {
+      title() {
+        return this.singer.name
+      },
+      bgImage() {
+        return this.singer.avatar
+      },
       ...mapGetters([
         'singer'
       ])
+    },
+    data() {
+      return {
+        songs: []
+      }
     },
     created() {
       this._getDetail()
@@ -26,10 +42,21 @@
           return
         }
         getSingerDetail(this.singer.id).then(res => {
-          if (res.code === ERR_OK) {
-            console.log(res.data.list)
+          if (res && res.code === ERR_OK) {
+            this.songs = this._normalizeSongs(res.data.list)
+            console.log(this.songs)
           }
         })
+      },
+      _normalizeSongs(list) {
+        let ret = []
+        list.forEach(item => {
+          let {musicData} = item
+          if (musicData.songid && musicData.albummid) {
+            ret.push(createSong(musicData))
+          }
+        })
+        return ret
       }
     }
   }
@@ -37,15 +64,6 @@
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
   @import "~common/stylus/variable"
-
-  .singer-detail
-    position: fixed
-    z-index: 100
-    top: 0
-    left: 0
-    right: 0
-    bottom: 0
-    background: $color-background
 
   .slide-enter-active, .slide-leave-active
     transition: all 0.3s
