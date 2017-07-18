88f0afbd1d1b0356c1629ec5666500182df0cd6f
Xun.Fu <fuxun@xiaoyouzi.com>
2017-07-18 16:26:00 +0800

    推荐项目的点击和显示。

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -14,7 +14,7 @@
         <div class="recommend-list">
           <h1 class="list-title">热门歌单推荐</h1>
           <ul>
-            <li v-for="item in discList" class="item">
+            <li v-for="item in discList" @click="selectItem(item)" class="item">
               <div class="icon">
                 <img v-lazy="item.imgurl" width="60" height="60" alt="item.creator.name">
               </div>
@@ -30,6 +30,7 @@
         <loading></loading>
       </div>
     </scroll>
+    <router-view></router-view>
   </div>
 </template>
 
@@ -40,6 +41,7 @@
   import {getRecommend, getDiscList} from 'api/recommend'
   import {ERR_OK} from 'api/config'
   import {playlistMixin} from 'common/js/mixin'
+  import {mapMutations} from 'vuex'
 
   export default {
     mixins: [playlistMixin],
@@ -65,6 +67,12 @@
         this.$refs.recommend.style.bottom = bottom
         this.$refs.scroll.refresh()
       },
+      selectItem(item) {
+        this.$router.push({
+          path: `/recommend/${item.dissid}`
+        })
+        this.setDisc(item)
+      },
       _getRecommend() {
         getRecommend().then(res => {
           if (res && res.code === ERR_OK) {
@@ -86,7 +94,10 @@
           this.$refs.scroll.refresh()
           this.checkLoaded = true
         }
-      }
+      },
+      ...mapMutations({
+        setDisc: 'SET_DISC'
+      })
     }
   }
 </script>
