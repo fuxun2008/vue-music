0b9b518351613dfcd1f9d718310e19d652935619
Xun.Fu <fuxun@xiaoyouzi.com>
2017-07-19 18:20:27 +0800

    完成排行页面的显示和接口数据获取。

--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -23,7 +23,7 @@
       ref="list"
     >
       <div class="song-list-wrapper">
-        <song-list :songs="songs" @select="selectItem"></song-list>
+        <song-list :songs="songs" :rank="rank" @select="selectItem"></song-list>
       </div>
       <div class="loading-container" v-show="!songs.length">
         <loading></loading>
@@ -58,6 +58,10 @@
       title: {
         type: String,
         default: ''
+      },
+      rank: {
+        type: Boolean,
+        default: false
       }
     },
     components: {
