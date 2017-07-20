30929878bca6a7952bce99065910b8d214484b4e
Xun.Fu <fuxun@xiaoyouzi.com>
2017-07-20 10:41:17 +0800

    添加suggest搜索建议组件和接口数据结果。

--- src/components/search/search.vue
+++ src/components/search/search.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="search">
     <div class="search-box-wrapper">
-      <search-box ref="searchBox"></search-box>
+      <search-box ref="searchBox" @query="onQueryChange"></search-box>
     </div>
-    <div class="shortcut-wrapper">
+    <div class="shortcut-wrapper" v-show="!query">
       <div class="shortcut">
         <div class="hot-key">
           <h1 class="title">热门搜索</h1>
@@ -15,21 +15,27 @@
         </div>
       </div>
     </div>
+    <div class="search-result" v-show="query">
+      <suggest :query=query></suggest>
+    </div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
   import SearchBox from 'base/search-box/search-box'
+  import Suggest from 'components/suggest/suggest'
   import {getHotKey} from 'api/search'
   import {ERR_OK} from 'api/config'
 
   export default {
     components: {
-      SearchBox
+      SearchBox,
+      Suggest
     },
     data() {
       return {
-        hotKey: []
+        hotKey: [],
+        query: ''
       }
     },
     created() {
@@ -38,6 +44,9 @@
     methods: {
       addQuery(query) {
         this.$refs.searchBox.setQuery(query.k)
+      },
+      onQueryChange(query) {
+        this.query = query
       },
       _getHotKey() {
         getHotKey().then(res => {
